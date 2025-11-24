7e68c5e5d7003e99d47d155309e641a174aa9a2c
Тралалело Тралала <timofei.marusev@ya.ru>
2025-11-24 23:56:29 +0300

    Added basic functions for getting data from Forge and Fabric .jar mod files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Minecraft-Unified-Hub-Team/RedstonePanel
 
 go 1.24.5
+
+require github.com/BurntSushi/toml v1.5.0
--- internal/modules/mod/list/list.go
+++ internal/modules/mod/list/list.go
@@ -1,10 +1,106 @@
 package list
 
 import (
+	"archive/zip"
 	"context"
-
+	"encoding/json"
+	"fmt"
+	"github.com/BurntSushi/toml"
 	"github.com/Minecraft-Unified-Hub-Team/RedstonePanel/internal/domain"
+	"io"
 )
+
+type modDataRawTOML struct {
+	DisplayName string `toml:"displayName"`
+	ModID       string `toml:"modId"`
+}
+
+type modDataArrayTOML struct {
+	Mods []modDataRawTOML `toml:"mods"`
+}
+
+func GetModDataFabric(file string) (domain.ModRef, error) {
+	r, err := zip.OpenReader(file)
+	if err != nil {
+		return domain.ModRef{}, fmt.Errorf("failed to access mod file: %s", file)
+	}
+	defer r.Close()
+
+	var targetJson *zip.File
+	for _, f := range r.File {
+		if f.Name == "fabric.mod.json" {
+			targetJson = f
+			break
+		}
+	}
+	if targetJson == nil {
+		return domain.ModRef{}, fmt.Errorf("failed to locate mod data at: %s", file)
+	}
+
+	rc, err := targetJson.Open()
+	if err != nil {
+		return domain.ModRef{}, fmt.Errorf("failed to access mod data at: %s", file)
+	}
+
+	data, err := io.ReadAll(rc)
+	if err != nil {
+		return domain.ModRef{}, fmt.Errorf("failed to read mod data at: %s", file)
+	}
+
+	var content map[string]interface{}
+	if err := json.Unmarshal(data, &content); err != nil {
+		return domain.ModRef{}, fmt.Errorf("JSON mod data is invalid at: %s", file)
+	}
+
+	name, ok := content["name"].(string)
+	if !ok {
+		return domain.ModRef{}, fmt.Errorf("failed to get mod name of: %s", file)
+	}
+
+	modid, ok := content["id"].(string)
+	if !ok {
+		return domain.ModRef{}, fmt.Errorf("failed to get mod ID of: %s", file)
+	}
+
+	return domain.ModRef{domain.ModID(modid), name, domain.MinecraftVersion(""), domain.URL("")}, nil
+}
+
+func GetModDataForge(file string) (domain.ModRef, error) {
+	r, err := zip.OpenReader(file)
+	if err != nil {
+		return domain.ModRef{}, fmt.Errorf("failed to access mod file: %s", file)
+	}
+	defer r.Close()
+
+	var targetToml *zip.File
+	for _, f := range r.File {
+		if f.Name == "META-INF/mods.toml" {
+			targetToml = f
+			break
+		}
+	}
+	if targetToml == nil {
+		return domain.ModRef{}, fmt.Errorf("failed to locate mod data at: %s", file)
+	}
+
+	rc, err := targetToml.Open()
+	if err != nil {
+		return domain.ModRef{}, fmt.Errorf("failed to access TOML mod data at: %s", file)
+	}
+
+	data0, err := io.ReadAll(rc)
+	if err != nil {
+		return domain.ModRef{}, fmt.Errorf("failed to read TOML mod data at: %s", file)
+	}
+
+	var dataArray modDataArrayTOML
+	if err := toml.Unmarshal(data0, &dataArray); err != nil {
+		return domain.ModRef{}, fmt.Errorf("TOML mod data is invalid at: %s", file)
+	}
+
+	return domain.ModRef{domain.ModID(dataArray.Mods[0].ModID), dataArray.Mods[0].DisplayName,
+		domain.MinecraftVersion(""), domain.URL("")}, nil
+}
 
 type ModList interface {
 	List(ctx context.Context, srv domain.ServerID) ([]domain.ModRef, error)
